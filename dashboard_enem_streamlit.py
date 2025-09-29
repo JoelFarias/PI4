eb4bc425d51e01ebc200f052c75531a9477746cc
Joel Carolino Farias <160360802+JoelFarias@users.noreply.github.com>
2025-09-28 21:55:18 -0300

    dashboard_enem_streamlit.py

--- dashboard_enem_streamlit.py
+++ dashboard_enem_streamlit.py
@@ -366,7 +366,7 @@ def create_parent_education_vs_mean_note(df):
             cnt = int(pivot_count.loc[y, x]) if x in pivot_count.columns and y in pivot_count.index else 0
             if np.isnan(mean_val):
                 # Use uma quebra de linha escapada (
-) dentro do f-string para evitar erro de sintaxe
+
                 row.append(f"Média: n/a
 Contagem: {cnt}")
             else:
